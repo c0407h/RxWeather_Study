eaa946d9f58de8b7ec57e5ebd33d80c1470782da
c0407h <67133244+c0407h@users.noreply.github.com>
2023-12-21 23:55:40 +0900

    위치서비스사용에 필요한 Delegate Proxy구현

--- RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
+++ RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
@@ -25,3 +25,64 @@ import UIKit
 import CoreLocation
 import RxSwift
 import RxCocoa
+
+//delegate proxy구현
+//extension 추가 CLLocation Manager에 hasDelegate 프로토콜 구현
+
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+    
+    
+}
+
+//DelegateProxy클래스 선언
+public class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    public init(locationManager: CLLocationManager) {
+        super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+    }
+    
+    
+    public static func registerKnownImplementations() {
+        self.register { RxCLLocationManagerDelegateProxy(locationManager: $0) }
+    }
+}
+
+
+//CLLocationManager에 Reactive Extention 추가
+
+extension Reactive where Base: CLLocationManager {
+    //델리게이트 속성 추가 앞에서 구현한 델리게이트 프록시
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: base)
+    }
+    
+    //didUpdateLocation 속성 추가
+    public var didUpdateLocation: Observable<[CLLocation]> {
+        
+        let sel = #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:))
+        
+        return delegate.methodInvoked(sel)
+            .map { parameters in
+            //gps로부터 새로운 위치정보가 전달되면 Cllocation 배열 방출
+            return parameters[1] as! [CLLocation]
+            }
+    }
+    
+    //속성 추가
+    public var didChangeAuthorizationStatus: Observable<CLAuthorizationStatus> {
+        //허가 상태가 변경되는 시점마다 현재 상태를 방출하도록 구현
+        let sel: Selector
+        if #available(iOS 14.0, *) {
+            sel = #selector(CLLocationManagerDelegate.locationManagerDidChangeAuthorization(_:))
+        } else {
+            sel = #selector(CLLocationManagerDelegate.locationManager(_:didChangeAuthorization:))
+        }
+        
+        return delegate.methodInvoked(sel)
+            .map { parameters in
+                return CLAuthorizationStatus(rawValue: parameters[1] as! Int32) ?? .notDetermined
+            }
+    }
+    
+}
+
